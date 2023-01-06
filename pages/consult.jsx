1a581b5e8ca746de10e0f11fa6a0f46902ad4c4b
jam-dev <jamalvga2002@gmail.com>
2023-01-07 01:39:03 +0530

    chat edited

--- pages/consult.jsx
+++ pages/consult.jsx
@@ -5,6 +5,7 @@ import Consultbanner from '../components/consult/Consultbanner1'
 import Ambulancebanner from '../components/ambulance/Ambulancebanner'
 import Medicinbanner from '../components/medicin/Medicinbanner'
 import Doctorcard from '../components/consult/Doctorcard'
+import Chat from '../components/consult/Chat'
 
 
 export default function consult() {
@@ -17,8 +18,8 @@ export default function consult() {
            <Navbar />
                 <div className="h-screen pt-2 pb-24 pl-2 pr-2 overflow-auto md:pt-0 md:pr-0 md:pl-0">
                     <div className="flex flex-col gap-5 flex-wrap sm:flex-row ">
-                          <Consultbanner />
                           <Doctorcard />
+                          <Chat />
                        {/* <Ambulancebanner />
                        <Medicinbanner /> */}
                     </div>
