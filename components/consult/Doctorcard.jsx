3694648d32c3a9e0b2304ec639d361bed7f161a9
jam-dev <jamalvga2002@gmail.com>
2023-01-07 01:31:26 +0530

    update

--- components/consult/Doctorcard.jsx
+++ components/consult/Doctorcard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Question from './question'
+import Card from './Card'
 
 export default function consultbaner() {
   return (
      <div className="w-full sm:w-1/2 xl:w-1/3">
                             <div className="">
                                 <div className="w-full h-screen bg-white rounded-t-2xl">
-                                    <div className="flex items-center justify-between mb-6">
-                                       
+                                    <div className="flex py-5 gap-4 flex-col px-5 items-center justify-between mb-6">
+                                       <Card />
+                                       <Card />
+                                       <Card />
+                      <Card />
+                       <Card />
                                      
                                     </div>
                                    
