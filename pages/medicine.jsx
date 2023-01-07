501f904de591474e14deccb50d24fcaeed941246
jam-dev <jamalvga2002@gmail.com>
2023-01-07 16:08:36 +0530

    bug fix

--- pages/medicine.jsx
+++ pages/medicine.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Sidebar from '../components/Sidebar/Sidebar'
+import Navbar from '../components/navbar/Navbar'
+import Sidebar from '../components/sidebar/Sidebar'
 import Medicin from '../components/medicin/Medicinfinder'
 
 
