11410dbca53129a7142532162c84eed22f6d2781
jam-dev <jamalvga2002@gmail.com>
2023-01-08 03:21:04 +0530

    button link added

--- components/consult/Question.jsx
+++ components/consult/Question.jsx
@@ -74,14 +74,16 @@ export default function Question() {
       
         { showScore?(
                   <div class="b mt-48 mb-12 animate-bounce mx-auto h-16 w-64 flex justify-center items-center">
-      <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-600 items-center rounded-xl shadow-2xl  cursor-pointer absolute overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out">
-      </div>
-            <Link href="/consult"class="text-center text-white font-semibold z-10  flex justify-content items-center">
-              <span class="">
+           <Link href="/consult" class=" text-white font-semibold z-10 flex justify-center items-center"> <div class="i h-16 w-64 bg-gradient-to-br from-blue-400 to-blue-600 items-center rounded-xl shadow-2xl flex justify-center  cursor-pointer overflow-hidden transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out">
+              
+                  <span class="">
                 <svg class="w-5 h-5 right-1.5 relative" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z" clip-rule="evenodd" />
                 </svg>
-              </span><span>Consult Now</span></Link>
+              </span><span>Consult Now</span>
+      </div>
+           </Link>
+            
               
     </div>
 
