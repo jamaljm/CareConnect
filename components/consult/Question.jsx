6b8da6dcfe5a25761bab45319efa361575d16863
jam-dev <jamalvga2002@gmail.com>
2023-01-07 10:10:27 +0530

    bug

--- components/consult/Question.jsx
+++ components/consult/Question.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
 import { useState } from 'react'
+import { Key } from 'react';
 
 export default function Question() {
 
@@ -87,7 +88,7 @@ export default function Question() {
     <div>
       <h3 className="mb-3 font-semibold text-gray-900 dark:text-white">{questions[currentQuestion].questionText}</h3>
 <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    {questions[currentQuestion].answerOptions.map((answerOption) => ( <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+    {questions[currentQuestion].answerOptions.map((answerOption) => ( <li key={answerOption.answerText} className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
         <div className="flex items-center pl-3">
             <input id="list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"/>
             <label for="list-radio-license" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{answerOption.answerText}</label>
