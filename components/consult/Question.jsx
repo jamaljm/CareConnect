65c9aae41ca4f6787ffc6362ce2ce1fa3320ca73
jam-dev <jamalvga2002@gmail.com>
2023-01-08 03:15:49 +0530

    some changes

--- components/consult/Question.jsx
+++ components/consult/Question.jsx
@@ -89,17 +89,17 @@ export default function Question() {
             ) : (
                 <>
     <div>
-      <h3 className="mb-3 font-semibold text-gray-900 dark:text-white">{questions[currentQuestion].questionText}</h3>
-<ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    {questions[currentQuestion].answerOptions.map((answerOption) => ( <li key={answerOption.answerText} className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+      <h3 className="mb-3 font-semibold text-gray-900 ">{questions[currentQuestion].questionText}</h3>
+<ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg ">
+    {questions[currentQuestion].answerOptions.map((answerOption) => ( <li key={answerOption.answerText} className="w-full border-b border-gray-200 rounded-t-lg">
         <div className="flex items-center pl-3">
             <input id="list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"/>
-            <label for="list-radio-license" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{answerOption.answerText}</label>
+            <label for="list-radio-license" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 ">{answerOption.answerText}</label>
         </div>
     </li>))}
    
           </ul>
-          <button type="button" onClick={() => handleAnswerOptionClick()} class="px-3 mt-3 py-2 text-md font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Next</button>
+          <button type="button" onClick={() => handleAnswerOptionClick()} class="px-3 mt-3 py-2 text-md font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Next</button>
 
                 </div >
                         </>
