4666638fb1596b58077678e84a97fda1b4e3512a
jam-dev <jamalvga2002@gmail.com>
2023-01-07 22:39:00 +0530

    change

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -29,10 +29,10 @@ export default function Signup() {
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
           <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+            <h2 className="text-3xl font-bold font-display leading-tight text-black sm:text-4xl">
               Sign Up Now
             </h2>
-            <p className="mt-2 text-base text-gray-600">
+            <p className="mt-2 mr-2 text-base font-body text-gray-600">
               Already have account
               <Link
                 href="/login"
@@ -46,7 +46,7 @@ export default function Signup() {
           
               <div className="space-y-5">
                 <div>
-                  <label className="text-base font-medium text-gray-900">
+                  <label className="text-base font-body font-medium text-gray-900">
                     {" "}
                     Email address{" "}
                   </label>
@@ -70,7 +70,7 @@ export default function Signup() {
 
                 <div>
                   <div className="flex items-center justify-between">
-                    <label className="text-base font-medium text-gray-900">
+                    <label className="text-base font-body font-medium text-gray-900">
                       {" "}
                       Password{" "}
                     </label>
@@ -96,7 +96,7 @@ export default function Signup() {
                 
                   <button
                     type="submit"
-                    className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
+                    className="inline-flex items-center justify-center w-full px-4 py-4 text-base font-body font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700"
                   >
                     Signup
                   </button>
@@ -107,7 +107,7 @@ export default function Signup() {
             <div className="mt-3 space-y-3">
               <button
                 type="button"
-                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
+                className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-body font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
               >
                 <div className="absolute inset-y-0 left-0 p-4">
                   <svg
