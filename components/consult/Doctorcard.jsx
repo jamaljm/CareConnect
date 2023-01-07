e1dee157e774af123b0437f978a1f66822e3dac5
jam-dev <jamalvga2002@gmail.com>
2023-01-07 10:33:50 +0530

    added

--- components/consult/Doctorcard.jsx
+++ components/consult/Doctorcard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from './Card'
+import Card from '../consult/Card'
 
 export default function consultbaner() {
   return (
--- components/doctors/Doctors.jsx
+++ components/doctors/Doctors.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../consult/Card1'
+import Card1 from '../consult/Card1'
 
 
 export default function Doctors() {
@@ -8,12 +8,12 @@ export default function Doctors() {
                             <div className="">
                                 <div className="w-full h-screen bg-white rounded-t-2xl">
                                     <div className="flex w-full py-5 gap-4 flex-col sm:flex-row flex-wrap px-5 items-center justify-start mb-6">
-                                       <Card />
-                                       <Card />
-                                       <Card />
-                      <Card />
-                       <Card />    <Card />
-                       <Card />
+                                       <Card1 />
+                                       <Card1 />
+                                       <Card1 />
+                      <Card1 />
+                       <Card1 />    <Card1 />
+                       <Card1 />
                                      
                                     </div>
                                    
